d4208f6a450b4f3ee57d76aa487ae15fec610a22
KacperPasnicki <101867400+KacperPasnicki@users.noreply.github.com>
2022-12-03 13:12:52 +0000

    Deploying to gh-pages from @ KacperPasnicki/goit-react-hw-05-movies@269b652eca049105d02e75d144d608d31f21e8b5 🚀

---
+++ static/js/216.8ca237a8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:function(e,r,s){s.r(r);var t=s(184);r.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h6",{className:"NotFound",children:"404"}),(0,t.jsx)("span",{className:"NotFoundText",children:"We are sorry... Something goes wrong"})]})}}}]);
+//# sourceMappingURL=216.8ca237a8.chunk.js.map
